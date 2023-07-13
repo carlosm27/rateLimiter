58104ce20312618c1c4c6fcf4fb27068f9e16b19
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-07-13 17:52:54 -0400

    Update FixedWindow.go

--- RateLimiter/FixedWindow.go
+++ RateLimiter/FixedWindow.go
@@ -17,20 +17,17 @@ func NewFixedWindowRateLimiter(limit int, windowSize time.Duration) *FixedWindow
 }
 
 func (r *FixedWindowRateLimiter) Allow() bool {
-	now := time.Now()
-	start := time.Unix(now.Unix(), 0)
-
 	// Count the number of requests in the current window.
 	requests := 0
 
-	for t := start; t.Before(now); t = t.Add(time.Second) {
+	for t := time.Now().Add(-r.WindowSize); t.Before(time.Now()); t = t.Add(time.Second) {
 		if t.After(t) {
 			requests++
 		}
 	}
 
-	// If the number of requests is less than the limit, then the request is allowed.
-	return requests < r.Limit
+	// If the number of requests is strictly less than the limit, then the request is allowed.
+	return requests <= r.Limit
 }
 
 func (r *FixedWindowRateLimiter) IsAllowedAt(t time.Time) bool {
